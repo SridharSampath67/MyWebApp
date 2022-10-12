ec21002504cfbbd031ec341de9222ddfad3a1fcd
unknown <Administrator@KANINI-LTP-353.gisbizdev.gisbiz.chennai.com>
2022-10-12 12:35:11 +0530

    Add all my files

---
+++ Member.Data/Interface/IMembers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Member.Data.Model;
+
+namespace Member.Data.Interface
+{
+    public interface IMembers
+    {
+        List<Members> GetAllMember();
+        Members GetMember(int id);
+
+        int SaveMember(Members members);
+
+        int UpdateMember(int id, Members members);
+
+        int DeleteMember(int id);
+    }
+}
---
+++ MyWebApp/Controllers/MemberController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Member.Data.Interface;
+using Member.Data.Model;
+using Member.Data.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MemberController : ControllerBase
+    {
+        private IMembers members = new MembersRepository();
+
+        [HttpGet("GetAllMembers")]
+        public ActionResult<IEnumerable<Members>> GetAllMembers()
+        {
+            return members.GetAllMember();
+
+        }
+        
+        [HttpGet("GetMemberById/{id}")]
+        public ActionResult<Members> GetMemberById(int id)
+        {
+            return members.GetMember(id);
+        }
+
+        [HttpPost("Members")]
+        public ActionResult<int> SaveMember(Members _members)
+        {
+            return members.SaveMember(_members);
+        }
+
+        [HttpPut("Members/{id}")]
+        public ActionResult<int> UpdateMember(int id, Members _members)
+        {
+            return members.UpdateMember(id, _members);
+        }
+
+        [HttpDelete("Members/{id}")]
+        public ActionResult<int> DeleteMember(int id)
+        {
+            return members.DeleteMember(id);
+        }
+    }
+}
